db211e826fc5cb9b4c684c0dbe7da83c58630d7b
Cody Barnes <codybarnes9624@gmail.com>
2024-10-02 22:32:35 -0400

    rough draft for ReadMe generator

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const { type } = require('os');
+const fs = require('fs').promises;
+
+const licenses = [
+    { name: 'MIT License', value: 'mit' },
+    { name: 'GNU General Public License (GPL) v3.0', value: 'gpl' },
+    { name: 'Apache License 2.0', value: 'apache' },
+    { name: 'Mozilla Public License 2.0', value: 'mozilla' },
+    { name: 'BSD 3-Clause License', value: 'bsd3' },
+    { name: 'BSD 2-Clause License', value: 'bsd2' },
+    { name: 'Creative Commons Zero v1.0 Universal (CC0)', value: 'cco' },
+    { name: 'Eclipse Public License 2.0', value: 'eclipse' },
+    { name: 'GNU Affero General Public License (AGPL) v3.0', value: 'agpl' },
+    { name: 'GNU Lesser General Public License (LGPL) v3.0', value: 'lgpl' },
+    { name: 'Unlicense', value: 'unlicense' },
+    { name: 'No license', value: '' }
+];
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+ return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your title for your ReadMe'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter your ReadMe description'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Enter your installation instructions'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select license type',
+        choices: licenses
+    },
+    {
+        type: 'input',
+        name: 'command',
+        message: 'Enter your start command for your ReadMe'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email'
+    }, 
+ ])
+};
+
+//enter proj titile
+//enter proj desc
+//enter install instruct
+//select license type
+//command to run project
+//enter email
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFile(fileName, data)
+    .then(() => console.log('Successfully created ReadMe'))
+    .catch((err) => console.log(err));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+    .then((answer) => writeToFile('ReadMe.md' , generateMarkdown(answer, licenses)))
+    .then(() => console.log("Answers added to ReadMe"))
+    .catch((err) => console.log(err));
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,113 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'mit':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    case 'gpl':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    case 'apache':
+      return '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    case 'mozilla':
+      return '![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)';
+    case 'bsd3':
+      return '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+    case 'bsd2':
+      return '![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)';
+    case 'cco':
+      return '![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)';
+    case 'eclipse':
+      return '![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)';
+    case 'agpl':
+      return '![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)';
+    case 'lgpl':
+      return '![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)';
+    case 'unlicense':
+      return '![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)';
+    default:
+      return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'mit':
+      return 'https://opensource.org/licenses/MIT';
+    case 'gpl':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    case 'apache':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'mozilla':
+      return 'https://opensource.org/licenses/MPL-2.0';
+    case 'bsd3':
+      return 'https://opensource.org/licenses/BSD-3-Clause';
+    case 'bsd2':
+      return 'https://opensource.org/licenses/BSD-2-Clause';
+    case 'cco':
+      return 'http://creativecommons.org/publicdomain/zero/1.0/';
+    case 'eclipse':
+      return 'https://opensource.org/licenses/EPL-1.0';
+    case 'agpl':
+      return 'https://www.gnu.org/licenses/agpl-3.0';
+    case 'lgpl':
+      return 'https://www.gnu.org/licenses/lgpl-3.0';
+    case 'unlicense':
+      return 'http://unlicense.org/';
+    case 'nolicense':
+      return '';
+    default:
+      return '';
+  }
+}
+
+
+function renderLicenseSection(license, choices) {
+  if (license === '' || license === 'nolicense') {
+    return { section: '', tableOfContents: '' };
+  } else {
+    const licenseName = choices.find(choice => choice.value === license)?.name;
+    const licenseSection = `## License
+  This project is licensed under the ${licenseName} - see the [LICENSE](${renderLicenseLink(license)}) file for details.`
+    const licenseTableOfContents = `\n- [License](#license)`
+    return { section: licenseSection, tableOfContents: licenseTableOfContents };
+  }
+}
+
+
+function generateMarkdown(data, choices) {
+  const { section: licenseSection, tableOfContents: licenseTableOfContents } = renderLicenseSection(data.license, choices);
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)${licenseTableOfContents}
+
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+Questions or comments? Check out my [GitHub](${data.github}) or shoot me an [email](mailto:${data.email}).
+
+${licenseSection}
 `;
 }
 
